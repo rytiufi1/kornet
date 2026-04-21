752c353ffa08c00cb5871faea395a1e14b5b6e1f
rytiufi1 <rytiufi1@gmail.com>
2026-04-21 13:49:29 +0200

    hj

--- 2016-roblox-main/components/navbar/components/loggedinArea.js
+++ 2016-roblox-main/components/navbar/components/loggedinArea.js
@@ -98,6 +98,7 @@ const useLoginAreaStyles = createUseStyles({
     listStyle: 'none',
   },
   ageNameContainer: {
+    float: "left",
     color: "#000 !important",
     marginRight: "10px",
     fontSize: "12px",
@@ -108,19 +109,25 @@ const useLoginAreaStyles = createUseStyles({
   },
   nameLink: {
     color: "#000 !important",
+    display: "inline",
     textDecoration: "none",
-    '&:hover $nameSpan': {
-      textDecoration: 'underline',
-    },
   },
   nameSpan: {
     fontWeight: 500,
+    color: "#000 !important",
+    display: "inline",
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
     "&:after": {
       content: '": "',
     },
   },
   ageSpan: {
     fontWeight: 500,
+    color: "#000 !important",
+    display: "inline",
   },
   messagesContainer: {
     height: "40px",
@@ -189,12 +196,12 @@ const LoggedInArea = () => {
     <div className={s.linkContainerCol}>
       <div className={`${s.row} row`}>
         <ul className={s.linkContainer}>
-          <li className={`${s.ageNameContainer} ${s.hideOnMobile}`}>
+          <div className={`${s.ageNameContainer} ${s.hideOnMobile}`}>
             <a href={`/users/${authStore.userId}/profile`} className={s.nameLink}>
               <span className={s.nameSpan}>{authStore.username}</span>
             </a>
             <span className={s.ageSpan}>13+</span>
-          </li>
+          </div>
 
           <li className={`${s.messagesContainer} ${s.hideOnMobile}`}>
             <a href="/My/Messages" className={s.messagesLink}>
