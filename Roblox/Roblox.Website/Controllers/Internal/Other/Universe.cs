cf3e1da4997f08079b36594ae16edf10470d3906
rytiufi1 <rytiufi1@gmail.com>
2026-04-22 23:26:11 +0200

    Reapply "Update Universe.cs"
    This reverts commit db04d4ee95f1dad667ecbb42c834a1c43be097ae.

--- Roblox/Roblox.Website/Controllers/Internal/Other/Universe.cs
+++ Roblox/Roblox.Website/Controllers/Internal/Other/Universe.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Dynamic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -146,33 +147,31 @@ namespace Roblox.Website.Controllers
         }
 
         [HttpGetBypass("v1/gametemplates")]
-        public dynamic GameTemplates()
+        public async Task<dynamic> GameTemplates()
         {
+            var templates = await services.games.MultiGetPlaceDetails(services.assets.getStarterPlaces.Values.ToList());
             return new
             {
-                data = new[]
+                data = templates.Select(c => new
                 {
-                    new
+                    gameTemplateType = "Generic",
+                    hasTutorials = false,
+                    universe = new
                     {
-                        gameTemplateType = "Generic",
-                        hasTutorials = false,
-                        universe = new
-                        {
-                            id = 95206881,
-                            name = "Baseplate",
-                            description = (string?)null,
-                            isArchived = false,
-                            rootPlaceId = 95206881,
-                            isActive = true,
-                            privacyType = "Public",
-                            creatorType = "User",
-                            creatorTargetId = 1,
-                            creatorName = "ROBLOX",
-                            created = "2013-11-01T08:47:14.07Z",
-                            updated = "2023-05-02T22:03:01.107Z"
-                        }
+                        id = c.universeId,
+                        name = c.name,
+                        description = c.description ?? "skbidii",
+                        isArchived = false,
+                        rootPlaceId = c.universeRootPlaceId,
+                        isActive = true,
+                        privacyType = "Public",
+                        creatorType = "User",
+                        creatorTargetId = c.builderId,
+                        creatorName = c.builder,
+                        created = c.created,
+                        updated = c.updated
                     }
-                }
+                })
             };
         }
 	}
