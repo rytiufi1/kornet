9da7ad87c6aa9e5c9ed42ebde6fa627f6e31eca9
rytiufi1 <rytiufi1@gmail.com>
2026-04-27 00:09:32 +0200

    HJKHJKLKJ

--- Roblox/Roblox.Website/Middleware/CorsMiddleware.cs
+++ Roblox/Roblox.Website/Middleware/CorsMiddleware.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Http.Extensions;
+using System.Text.RegularExpressions;
 
 namespace Roblox.Website.Middleware;
 
 public class RobloxPlayerCorsMiddleware
 {
     private RequestDelegate _next;
+    private static readonly Regex AllowedOriginPattern = new(@"^https://(([a-z0-9-]+\.)*)?kornet\.lat$", RegexOptions.Compiled);
+
     public RobloxPlayerCorsMiddleware(RequestDelegate next)
     {
         _next = next;
@@ -27,10 +30,28 @@ public class RobloxPlayerCorsMiddleware
         
         return "default-src 'self'; img-src https://kornet.lat http://kornet.lat https://*.kornet.lat "+imgSrc+"; child-src 'self'; script-src https://esm.sh "+scriptSrc+"; frame-src 'self' https://hcaptcha.com https://*.hcaptcha.com https://*.kornet.lat https://kornet.lat; style-src 'unsafe-inline' 'self' https://fonts.googleapis.com https://hcaptcha.com https://*.hcaptcha.com https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css https://css.rbxcdn.com; font-src 'self' fonts.gstatic.com https://css.rbxcdn.com; connect-src "+connectSrc+"; worker-src 'self';";
     }
+
     // we dont need shitty ass comments that are useless :skull:
     public async Task InvokeAsync(HttpContext ctx)
     {
-        var isAuthenticated = ctx.Items.ContainsKey(".ROBLOSECURITY");
+        var origin = ctx.Request.Headers["Origin"].ToString();
+
+        if (!string.IsNullOrEmpty(origin) && AllowedOriginPattern.IsMatch(origin))
+        {
+            ctx.Response.Headers["Access-Control-Allow-Origin"] = origin;
+            ctx.Response.Headers["Access-Control-Allow-Credentials"] = "true";
+            ctx.Response.Headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, PATCH, OPTIONS";
+            ctx.Response.Headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Requested-With";
+            ctx.Response.Headers["Vary"] = "Origin";
+
+            if (ctx.Request.Method == "OPTIONS")
+            {
+                ctx.Response.StatusCode = 204;
+                return;
+            }
+        }
+
+        var isAuthenticated = ctx.Request.Cookies.ContainsKey(".ROBLOSECURITY");
         ctx.Response.Headers["Cross-Origin-Opener-Policy"] = "same-origin";
         ctx.Response.Headers["Cross-Origin-Resource-Policy"] = "cross-origin";
         ctx.Response.Headers["X-Frame-Options"] = "SAMEORIGIN";
